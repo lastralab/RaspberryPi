9d49d97bf3966e59b19e097e34993071287a37ef
Niam Moltta <lastralab@gmail.com>
2017-09-26 19:01:19 -0300

    Update PWMexample.py

--- PWMexample.py
+++ PWMexample.py
@@ -1,21 +1,32 @@
 # Author: Niam Moltta
+# UY - 2017
+
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
+
 pwm = GPIO.PWM(12, 50)
 pwm.start(0)
+
 print("Let's fade!")
+
 try:
+    
     while True:
+        
         for i in range(100):
             pwm.ChangeDutyCycle(i)
             time.sleep(0.02)
+            
         for i in range(100, 0, -1):
             pwm.ChangeDutyCycle(i)
             time.sleep(0.02)
+            
 except KeyboardInterrupt:
+    
     GPIO.cleanup() #Ctrl+C to exit
     print("Good bye!")
 
